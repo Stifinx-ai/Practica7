8380066c351dd0a000a131d552a2517f5be41aeb
Stifinx-ai <yarethavf@gmail.com>
2025-03-27 14:03:20 -0600

    Create Ejercicio1.c

---
+++ Ejercicio1.c
@@ -0,0 +1,23 @@
+// Avila Flores Ahmed
+//PRACTICA 7; estructuras de selección 
+// ejercicio 1; vaidación de a mayor b
+
+
+#include <stdio.h>
+
+void main ()
+{
+    int a, b ;
+    
+    printf("Dame un numero: ");
+    scanf("%i", &a);
+    printf("Dame otro numero: ");
+    scanf("%i", &b);
+    
+    if(a>b)
+    {
+        printf("a es mayor a b \n");
+    }
+    
+    printf("fin del programa \n");
+}
